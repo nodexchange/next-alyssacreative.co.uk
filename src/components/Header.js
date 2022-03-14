25dcf77ba6408bef2ae598ea9d0038370b8808e5
Marcin - Windows 11 <nodexchange@outlook.com>
2022-03-14 17:36:39 +0000

    feat: cross system setup + social icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,26 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import Router from 'next/router'
-import { Logo } from '@/components/Logo'
+import Link from "next/link";
+import Image from "next/image";
+import Router from "next/router";
+import { Logo } from "@/components/Logo";
 // import { Dialog } from '@headlessui/react'
-import { useEffect, useState } from 'react'
-import clsx from 'clsx'
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+import SocialIcons from "./SocialIcons";
 // import { ThemeSelect, ThemeToggle } from './ThemeToggle'
 
-export function NavPopover({ display = 'md:hidden', className, ...props }) {
-  let [isOpen, setIsOpen] = useState(false)
+export function NavPopover({ display = "md:hidden", className, ...props }) {
+  let [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    if (!isOpen) return
+    if (!isOpen) return;
     function handleRouteChange() {
-      setIsOpen(false)
+      setIsOpen(false);
     }
-    Router.events.on('routeChangeComplete', handleRouteChange)
+    Router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      Router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [isOpen])
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [isOpen]);
 
   return (
     <div className={clsx(className, display)} {...props}>
@@ -80,7 +81,7 @@ export function NavPopover({ display = 'md:hidden', className, ...props }) {
         </div>
       </Dialog> */}
     </div>
-  )
+  );
 }
 /*
 HOME ABOUT US SERVICES
@@ -106,31 +107,39 @@ export function NavItems() {
       </li>
       <li>
         <Link href="/docs/installation">
-          <a className="hover:text-sky-500 dark:hover:text-sky-400">Contact us</a>
+          <a className="hover:text-sky-500 dark:hover:text-sky-400">
+            Contact us
+          </a>
         </Link>
       </li>
     </>
-  )
+  );
 }
 
-export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section }) {
-  let [isOpaque, setIsOpaque] = useState(false)
+export function Header({
+  hasNav = false,
+  navIsOpen,
+  onNavToggle,
+  title,
+  section,
+}) {
+  let [isOpaque, setIsOpaque] = useState(false);
 
   useEffect(() => {
-    let offset = 50
+    let offset = 50;
     function onScroll() {
       if (!isOpaque && window.scrollY > offset) {
-        setIsOpaque(true)
+        setIsOpaque(true);
       } else if (isOpaque && window.scrollY <= offset) {
-        setIsOpaque(false)
+        setIsOpaque(false);
       }
     }
-    onScroll()
-    window.addEventListener('scroll', onScroll, { passive: true })
+    onScroll();
+    window.addEventListener("scroll", onScroll, { passive: true });
     return () => {
-      window.removeEventListener('scroll', onScroll, { passive: true })
-    }
-  }, [isOpaque])
+      window.removeEventListener("scroll", onScroll, { passive: true });
+    };
+  }, [isOpaque]);
 
   return (
     <>
@@ -142,7 +151,8 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
               src="/logo-1.jpeg"
               alt=""
               className="w-[71.75rem] flex-none max-w-none dark:hidden"
-              width={72} height={16}
+              width={72}
+              height={16}
             />
           </picture>
           <picture>
@@ -151,25 +161,26 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
               src="/logo-1.jpeg"
               alt=""
               className="w-[90rem] flex-none max-w-none hidden dark:block"
-              width={72} height={16}
+              width={72}
+              height={16}
             />
           </picture>
         </div>
       </div>
       <div
         className={clsx(
-          'sticky top-0 z-40 w-full backdrop-blur flex-none transition-all duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06]',
+          "sticky top-0 z-40 w-full backdrop-blur flex-none transition-all duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06]",
           isOpaque
-            ? 'bg-white supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75'
-            : 'bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent'
+            ? "bg-white supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75"
+            : "bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent"
         )}
       >
         <div className="max-w-8xl mx-auto">
           <div
             className={clsx(
-              'transition-all duration-500 border-b border-gray-900/10 lg:px-8 lg:border-0 dark:border-gray-300/10',
-              hasNav ? 'mx-4 lg:mx-0' : 'px-4',
-              isOpaque ? 'py-0' : 'py-2'
+              "transition-all duration-500 border-b border-gray-900/10 lg:px-8 lg:border-0 dark:border-gray-300/10",
+              hasNav ? "mx-4 lg:mx-0" : "px-4",
+              isOpaque ? "py-0" : "py-2"
             )}
           >
             <div className="relative flex items-center">
@@ -177,8 +188,8 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
                 <a
                   className="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto"
                   onContextMenu={(e) => {
-                    e.preventDefault()
-                    Router.push('/brand')
+                    e.preventDefault();
+                    Router.push("/brand");
                   }}
                 >
                   <span className="sr-only">Alyssa Salazar creatives</span>
@@ -193,23 +204,19 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
                 </nav>
                 <div className="flex items-center border-l border-gray-200 ml-6 pl-6 dark:border-gray-800">
                   {/* <ThemeToggle panelClassName="mt-8" /> */}
+                  <SocialIcons />
                   <a
                     href="https://github.com/tailwindlabs/tailwindcss"
                     className="ml-6 block text-gray-400 hover:text-gray-500 dark:hover:text-gray-300"
                   >
                     <span className="sr-only">AS Creative on social</span>
-                    <svg
-                      viewBox="0 0 16 16"
-                      className="w-5 h-5"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
-                    </svg>
                   </a>
                 </div>
               </div>
-              <NavPopover className="flex items-center ml-auto" display="lg:hidden" />
+              <NavPopover
+                className="flex items-center ml-auto"
+                display="lg:hidden"
+              />
             </div>
           </div>
           {hasNav && (
@@ -261,5 +268,5 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
         </div>
       </div>
     </>
-  )
+  );
 }
